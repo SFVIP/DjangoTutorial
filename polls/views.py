6a25a3cd04a65dad67ccec7e5c44a94cb818eb41
Simon Foo <simonfookp@gmail.com>
2025-12-23 23:27:15 +0800

    Added get_object_or_404, Polls app_name, and remove hardcoding in href. Edited polls detail template.

--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,20 @@
 from django.http import Http404, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
 
 # Create your views here.
 def index(req):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    latest_question_list = Question.objects.order_by("pub_date")[:5]
     context = {"latest_question_list": latest_question_list}
     return render(req, "polls/index.html", context)
 
 
 def detail(req, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(req, "polls/detail.html", {"question": question})
+
 
 def results(req, question_id):
     response = f"You are looking at the results of question {question_id}."
