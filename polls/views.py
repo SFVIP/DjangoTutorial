dfa000a7a0062aebf5da322e180a6df5d5a92683
Simon Foo <simonfookp@gmail.com>
2025-12-23 22:41:21 +0800

    Added detail, results, vote of views and urls.

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,16 @@ from django.shortcuts import render
 # Create your views here.
 def index(req):
     return HttpResponse("Hello World! You are at the polls index!")
+
+
+def detail(req, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+
+def results(req, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(req, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
