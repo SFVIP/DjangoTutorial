ab88246b8cd5c9f7cb97020f3b85a0ea02671d07
Simon Foo <simonfookp@gmail.com>
2025-12-28 18:42:23 +0800

    Changed from redundant View into built-in GenericView, updated polls Url for GenericView.

--- polls/views.py
+++ polls/views.py
@@ -1,26 +1,32 @@
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 
 # Create your views here.
-def index(req):
-    latest_question_list = Question.objects.order_by("pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-    return render(req, "polls/index.html", context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    # if context_object_name is not defined,
+    # need to use "question_list" as the variable inside the template
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published question."""
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def detail(req, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(req, "polls/detail.html", {"question": question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
 
-def results(req, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(req, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(req, question_id):
