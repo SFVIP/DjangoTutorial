b4825adbe8ee01298569145a1cf74d3148bb0e74
Simon Foo <simonfookp@gmail.com>
2026-01-10 22:21:00 +0800

    Added header for Polls pub_date.

--- polls/admin.py
+++ polls/admin.py
@@ -4,7 +4,10 @@ from .models import Question
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["question_text", "pub_date"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
 
 
 admin.site.register(Question, QuestionAdmin)
